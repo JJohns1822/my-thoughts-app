3e6ad4914278daaf7570b26ee7e5666128950904
jeff <jjohnston1822@gmail.com>
2021-02-16 21:47:13 -0500

    final-commit

---
+++ src/app/social-posts/social-posts.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../interfaces/post';
+
+@Component({
+  selector: 'app-social-posts',
+  templateUrl: './social-posts.component.html',
+  styleUrls: ['./social-posts.component.css'],
+})
+export class SocialPostsComponent implements OnInit {
+  myPosts: Post[] = [
+    {
+      title: 'I am hungry',
+      thoughts: 'If I had a cheeseburger right now I would be soooooo happy',
+    },
+    {
+      title: 'How far away are the stars?',
+      thoughts: 'Probably pretty far',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  onSubmit = (post: Post) => {
+    this.myPosts.unshift(post);
+  };
+
+  onDelete = (index: number): void => {
+    this.myPosts.splice(index, 1);
+  };
+}
